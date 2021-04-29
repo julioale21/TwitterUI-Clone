4ea48072be835b37055f37a349a3c1485ede2508
Julio Romero <julio_ale21@hotmail.com>
2021-04-29 14:51:51 -0300

    chore: removed message component content

--- src/screens/Messages.tsx
+++ src/screens/Messages.tsx
@@ -1,27 +1,7 @@
-import { Stack, SkeletonText, SkeletonCircle, Skeleton } from "@chakra-ui/react";
 import React from "react";
 
-const TWEETS = Array(12)
-  .fill(true)
-  .map((_, index) => index);
-
 const MessagesScreen: React.FC = () => {
-  return (
-    <Stack maxHeight="100vh" overflowY="auto" paddingY={12} spacing={6}>
-      {TWEETS.map((index) => (
-        <Stack key={index} spacing={4}>
-          <Stack alignItems="center" direction="row" spacing={4}>
-            <SkeletonCircle height={12} width={12} />
-            <Stack>
-              <Skeleton height={6} width="120px" />
-              <Skeleton height={3} width="80px" />
-            </Stack>
-          </Stack>
-          <SkeletonText height={36} noOfLines={6} />
-        </Stack>
-      ))}
-    </Stack>
-  );
+  return <div>{`<MessageScreen />`}</div>;
 };
 
 export default MessagesScreen;
